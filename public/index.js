d01bda2b2b68eacfa8eb68e34544b6f4e02d7b83
name <email>
2022-12-23 16:24:59 +0100

    update ws link

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-let socket = new WebSocket("ws://192.168.31.62:3000/");
+let socket = new WebSocket("ws://10.217.9.57:10000/");
 
 socket.onmessage = function (event) {
   document.getElementById("seconds").innerText = event.data;
