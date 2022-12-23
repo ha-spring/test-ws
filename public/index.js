a475dcfa0d80ac226ce2cec4ed60fb91e8b83fb2
name <email>
2022-12-23 16:30:21 +0100

    get location.host

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-let socket = new WebSocket("ws://10.217.9.57:10000/");
+let socket = new WebSocket(`ws://${location.host}/`);
 
 socket.onmessage = function (event) {
   document.getElementById("seconds").innerText = event.data;
